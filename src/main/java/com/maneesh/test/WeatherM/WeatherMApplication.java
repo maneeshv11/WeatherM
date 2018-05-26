633714ad63c9d46e0b19c52555b15499f6f220a9
Maneesh Verma <maneeshv11@gmail.com>
2018-05-26 17:38:37 +0530

    initial commit

---
+++ src/main/java/com/maneesh/test/WeatherM/WeatherMApplication.java
@@ -0,0 +1,12 @@
+package com.maneesh.test.WeatherM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherMApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherMApplication.class, args);
+	}
+}
